ce5cb4d574a00b8fe2366b1ad8bf61034097c749
Ilja Mogilnõi <iljamog@users.noreply.github.com>
2019-09-15 18:23:31 +0300

    ifid lisatud

--- tund02/try.php
+++ tund02/try.php
@@ -3,8 +3,20 @@ $userName = "Ilja Mogilnõi";
 $fullTimeNow = date("d.m.Y H:i:s");
 $hourNow = date ("H");
 $partOfDay = "hägune aeg";
+if($hourNow == 1 && $hourNow <= 5){
+	$partOfDay = "peaksid sa magama";
+}
 if($hourNow < 8){
-$partOfDay = "varajane hommik";
+	$partOfDay = "varajane hommik";
+}
+if($hourNow > 8 && $hourNow < 10){
+	$partOfDay = "hommik";
+}
+if($hourNow >= 10){
+	$partOfDay = "päev";
+}
+if($hourNow >= 17){
+	$partOfDay = "õhtu";
 }
 ?>
 <!DOCTYPE html>
@@ -35,9 +47,17 @@ $partOfDay = "varajane hommik";
 	<?php
 	echo "<p> Lehe avamise hetkel oli " .$partOfDay .".</p>";
 	?>
-	  
-  <img src="funycat.jpg" alt="kassipilt" />
-  
+	<img src="funycat.jpg" alt="kassipilt" />
+	<?php
+$d = date("D");
+if($d == "Fri"){
+    echo "Mõnusat nädalavahetust!";
+} elseif($d == "Sun"){
+    echo "Ilusat pühapäeva!";
+} else{
+    echo "Ilusat päeva!";
+}
+?>	   
   <hr>
 </body>
 </html> 
